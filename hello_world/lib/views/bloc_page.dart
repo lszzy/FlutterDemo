3694bc8be292cfea10ce84c8cb15e8a0e1456f3a
wuyong <245593358@qq.com>
2020-01-02 17:31:10 +0800

    fix

--- hello_world/lib/views/bloc_page.dart
+++ hello_world/lib/views/bloc_page.dart
@@ -1,29 +1,23 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 
-class BlocPage extends StatelessWidget {
+class BlocPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _BlocPageState();
+}
+
+class _BlocPageState extends State<BlocPage> {
+  int _counter = 0;
+  final StreamController<int> _streamController = StreamController<int>();
+
+  @override
+  void dispose() {
+    _streamController.close();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    void _onClickStream() {
-      final StreamController ctrl = StreamController();
-      final StreamSubscription subscription = ctrl.stream.listen((data){
-        print("$data");
-      });
-
-      ctrl.sink.add('test string');
-      ctrl.sink.add(123);
-      ctrl.close();
-    }
-
-    void _onClickBroadCast() {
-      final StreamController<int> ctrl = StreamController<int>.broadcast();
-      final StreamSubscription subscription = ctrl.stream.where((value) => (value % 2 == 0)).listen((value) => print('$value'));
-      for (int i=1; i<11; i++) {
-        ctrl.sink.add(i);
-      }
-      ctrl.close();
-    }
-
     return Scaffold(
       appBar: AppBar(
         title: Text("Bloc"),
@@ -31,6 +25,18 @@ class BlocPage extends StatelessWidget {
       body: Center(
         child: Column(
           children: <Widget>[
+            RaisedButton(
+              child: StreamBuilder<int>(
+                stream: _streamController.stream,
+                initialData: _counter,
+                builder: (BuildContext context, AsyncSnapshot<int> snapshot){
+                  return Text("Counter ${snapshot.data}");
+                },
+              ),
+              onPressed: (){
+                _streamController.sink.add(++_counter);
+              },
+            ),
             RaisedButton(
               child: Text("Stream"),
               onPressed: _onClickStream,
@@ -43,5 +49,25 @@ class BlocPage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  void _onClickStream() {
+    final StreamController ctrl = StreamController();
+    final StreamSubscription subscription = ctrl.stream.listen((data){
+      print("$data");
+    });
+
+    ctrl.sink.add('test string');
+    ctrl.sink.add(123);
+    ctrl.close();
+  }
+
+  void _onClickBroadCast() {
+    final StreamController<int> ctrl = StreamController<int>.broadcast();
+    final StreamSubscription subscription = ctrl.stream.where((value) => (value % 2 == 0)).listen((value) => print('$value'));
+    for (int i=1; i<11; i++) {
+      ctrl.sink.add(i);
+    }
+    ctrl.close();
   }
 }
